a8f90ceb44915250a87bd55cfc352c50763239e4
James Viall <jamesviall@gmail.com>
2018-06-24 20:52:33 -0500

    Printing all open trucks, formatted output.

--- FoodTruckFinder.js
+++ FoodTruckFinder.js
@@ -7,11 +7,20 @@ app.run = function () {
     time = date.getHours() + ':' + ((date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes()),
     day = date.getDay(),
     result;
+
   request('http://data.sfgov.org/resource/bbb8-hzi6.json?$query=select location, applicant as name where start24 < \'' + time + '\' and end24 > \'' + time + '\' and dayorder=' + day + ' order by name', function (error, response, body) {
     result = JSON.parse(body);
-    console.log(result);
+    app.displayOpenTrucks(result);
   });
 }
+
+app.displayOpenTrucks = function (data) {
+  process.stdout.write('NAME'.padEnd(80) + 'ADDRESS\n-------------------\n');
+  data.forEach(truck => {
+    process.stdout.write(truck.name.padEnd(80) + truck.location + '\n');
+  });
+}
+app.run();
 
 // to run locally, first install node and npm. then:
 // $ npm install request && node FoodTruckFinder.js
