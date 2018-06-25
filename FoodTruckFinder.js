951cafa489a81a15b6fa258204624a3c3014e0c9
James Viall <jamesviall@gmail.com>
2018-06-24 23:52:32 -0500

    Edge case behavior and basic error handling. Commenting. README. Mild refactoring and ouptut formatting.

--- FoodTruckFinder.js
+++ FoodTruckFinder.js
@@ -1,51 +1,86 @@
 const request = require('request');
 
-var app = {};
+let app = {};
 
+  /**
+   * Database Request
+   * SoQL: Select Location and Name (applicant) from rows where
+   * today's time is after their open time and before their close time
+   * order by name, alpha-numerically  
+   */
 app.run = function () {
   let date = new Date(),
     time = date.getHours() + ':' + ((date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes()),
     day = date.getDay(),
-    result;
+    DoW = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    results;
 
   request('http://data.sfgov.org/resource/bbb8-hzi6.json?$query=select location, applicant as name where start24 < \'' + time + '\' and end24 > \'' + time + '\' and dayorder=' + day + ' order by name', function (error, response, body) {
-    result = JSON.parse(body);
-    app.displayOpenTrucks(result);
+    try {
+      results = JSON.parse(body); // throws syntax error
+
+      if(results.length === 0 || !results.length){// empty query
+        process.stdout.write("\nNo food trucks are open at this time, exiting.\n");
+        process.exit();
+      }
+      process.stdout.write('Displaying Food Trucks open at ' + time + ' on ' + DoW[day] + '. (' + results.length + ' total)\n');
+      app.displayOpenTrucks(results);
+    }catch(e){
+      console.log(e + "\nAn error occurred--exiting.");
+      process.exit(1);
+    }
   });
 }
 
+/**
+ * Makes the first call to printPage(), handles the command line input, and
+ * calls printPage() or exits depending on user input of yes/no.
+ * @param data -- the array of food truck "objects"
+ */
 app.displayOpenTrucks = function (data) {
   let offset = 0;
-  app.printMoreTrucks(offset, data);
+  app.printPage(offset, data);
 
   process.stdin.setEncoding('utf8');
-  process.stdout.write('Display more trucks? (yes/no) >> ');
+  process.stdout.write('\nDisplay more trucks? (yes/no) >> ');
 
-  // command line input
+  // CL input listener
   process.stdin.on('data', function (input) {
     input = input.toString().trim().toLowerCase();
     process.stdin.pause();
+
     if (input === 'no' || input === 'n') {
-      process.stdout.write('Exiting\n');
+      process.stdout.write('Exiting');
       process.exit();
     }
     else if (input === 'yes' || input === 'y') {
-      app.printMoreTrucks(++offset, data);
+      app.printPage(++offset, data);
     }
-    process.stdout.write('Display more trucks? (yes/no) >> ');
+    process.stdout.write('\nDisplay more trucks? (yes/no) >> ');
     process.stdin.resume();
   });
 }
 
-app.printMoreTrucks = function (offset, data) {
+/**
+ * Prints 1 page of up to 10 Food trucks' name and location. Exits the
+ * program if no more trucks are left.
+ * @param offset -- page offset for the data array 
+ * @param data -- the array of food truck "objects"
+ */
+app.printPage = function (offset, data) {
   for (let i = 0; i < 10; i++) {
     let index = offset * 10 + i;
-    if (!data[index]) {
-      process.stdout.write('No more results, exiting.\n');
+    if (!data[index]) { // next index is undefined
+      process.stdout.write('\nNo more results--exiting.');
       process.exit();
     }
-    if (i === 0) process.stdout.write('NAME'.padEnd(80) + 'ADDRESS\n-------------------\n');
+    if (i === 0) process.stdout.write('NAME'.padEnd(80) + 'ADDRESS\n'.padEnd(95, '-') + '\n');
     process.stdout.write(data[index].name.padEnd(80) + data[index].location + '\n');
+  }
+  // no point in prompting user if no trucks are left to print.
+  if (!data[offset*10 + 10]) { 
+    process.stdout.write('\nNo more results--exiting.');
+    process.exit();
   }
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# SanFran Food Truck Finder
+# San-Fran Food Truck Finder
+This program queries the [public database](https://data.sfgov.org/Economy-and-Community/Mobile-Food-Schedule/jjew-r69b) of Food Trucks in the San Francisco area and displays the trucks open at runtime.
+
+## Run
+Requires installation of Node and NPM. Run `npm install` to install the project's dependencies.
+
+Run the command `npm start` within the project directory to run the program.
+
 
